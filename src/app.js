144d8b8bdefc44ca314f28ce7a6617c78f46ea9b
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-04 13:11:00 -0500

    Updating javascript code

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
-let tokyo = moment().tz("Asia/Tokyo");
-let paris = moment().tz("Europe/Paris");
-let sydney = moment().tz("Australia/Sydney");
-let lisbon = moment().tz("Europe/Lisbon");
-let new_york = moment().tz("America/New_York");
-let toronto = moment().tz("America/Toronto");
-let seoul = moment().tz("Asia/Seoul");
-let madeira = moment().tz("Atlantic/Madeira");
-
-let cities_array = {
-  tokyo: tokyo,
-  paris: paris,
-  sydney: sydney,
-  lisbon: lisbon,
-  new_york: new_york,
-  toronto: toronto,
-  seoul: seoul,
-  madeira: madeira,
-};
-
+let city = document.querySelector("#actual_city");
+let hour = document.querySelector("#actual_hour");
+let date = document.querySelector("#actual_date");
 let city_container = document.querySelector("#grid-container");
-let countrys = document.querySelector("#countrys");
-countrys.addEventListener("change", function (event, index) {
-  cities_value = event.target.value;
-  if (event.target.value.length > 0) {
+let dropdown = document.querySelector("#countries");
+
+function citiesTiming(value) {
+  let hour = moment().tz(value).format("h:mm:ss");
+  return setInterval(hour, 1000);
+}
+
+function citiesDate(value) {
+  let date = moment().tz(value).format("MMMM Do YYYY");
+  return date;
+}
+
+let x = dropdown.addEventListener("change", function (event) {
+  let selectedValue = event.target.value;
+  if (selectedValue.length > 0) {
+    let citiesname = selectedValue.replace("", " ").split("/")[1];
     city_container.innerHTML += `<div class="cities-preview" id="cities-preview">
-            <div class="city">${cities_value}</div>
-            <div class="hour">${cities_array[cities_value].format("YYYY")}</div>
-            <div class="date">${cities_array[cities_value].format("dddd")}</div>
-          </div>`;
+            <div class="city">${citiesname}</div>
+            <div class="hour">${citiesTiming(selectedValue)}</div>
+            <div class="date">${citiesDate(selectedValue)}</div>
+          </div> 
+          </br >`;
   }
 });
