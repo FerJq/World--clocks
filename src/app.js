dd3d0f44e8b372190ea3bfd2e347572b635a5730
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-05 14:40:22 -0500

    new file, all cities function
    New file html, for all cities
    night mode deploy
    all cities and back buttons working in same page

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ let dateC = document.querySelector("#actual_date");
 let citySelected = document.getElementById("city");
 let hourSelected = document.getElementById("hour");
 let dateSelected = document.getElementById("date");
+
 let current = document.getElementById("selected");
 let dropdown = document.querySelector("#countries");
 let display_all = document.getElementById("displayAllCities");
-let getAll = document.getElementById("showAll");
+let getAll = document.getElementById("all");
+let displaycity = document.querySelector(".displaycity");
+//Time Zones
 let asia_tokyo = "Asia/Tokyo";
 let europe_paris = "Europe/Paris";
 let aus_sydn = "Australia/Sydney";
@@ -17,94 +20,94 @@ let asia_seoul = "Asia/Seoul";
 let at_mad = "Atlantic/Madeira";
 let ame_ny = "America/New_York";
 
+let theme_bt = document.getElementById("mode");
+let body = document.querySelector("body");
+theme_bt.addEventListener("click", function () {
+  if (body.classList.contains("darkmode")) {
+    body.classList.remove("darkmode");
+  } else {
+    body.classList.add("darkmode");
+  }
+});
 
 function allCities() {
   let pieceHTML = `<div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Tokyo</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Tokyo</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(asia_tokyo)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(asia_tokyo)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Paris</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Paris</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(europe_paris)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(europe_paris)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Sydney</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Sydney</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(aus_sydn)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(aus_sydn)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Lisbon</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Lisbon</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(eu_lisbon)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(eu_lisbon)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Toronto</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Toronto</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(ame_toro)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(ame_toro)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Seoul</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Seoul</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(asia_seoul)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(asia_seoul)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">Madeira</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">Madeira</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(at_mad)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(at_mad)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
           <div class="cities-preview" id="cities-preview">
-            <div class="city" id="city">New York</div>
-            <div class="hour" id="hour">${moment()
+            <div class="city_all" id="city">New York</div>
+            <div class="hour_all" id="hour">${moment()
               .tz(ame_ny)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
-            <div class="date" id="date"${moment()
+            <div class="date_all" id="date">${moment()
               .tz(ame_ny)
-              .format("MMMM Do YYYY")}></div>
+              .format("MMMM Do YYYY")}</div>
           </div>
 `;
-
-  return pieceHTML;
+  display_all.innerHTML = pieceHTML;
 }
 
-getAll.addEventListener("click", function (event) {
-  function interDisplay() {
-    display_all.innerHTML = allCities();
-  }
-  setInterval(interDisplay, 1000);
-  event.preventDefault();
-  let label = document.getElementById("selected");
-  label.innerHTML = `˜”*°•.All Cities.•°*”˜`;
-});
+setInterval(allCities, 1000);
 
 function currentLocation() {
   let actual_location = moment.tz.guess();
@@ -137,9 +140,11 @@ dropdown.addEventListener("change", function (event) {
     function upd() {
       let citiesname = values_selection.replace("_", " ").split("/")[1];
       selected.innerHTML = "˜”*°•.City.•°*”˜";
-      citySelected.innerHTML = citiesname;
-      hourSelected.innerHTML = citiesTiming();
-      dateSelected.innerHTML = citiesDate();
+      displaycity.innerHTML = `<div class="cities-preview" id="displayAllCities">
+            <div class="cityy" id="city">${citiesname}</div>
+            <div class="hourr" id="hour">${citiesTiming()}</div>
+            <div class="datee" id="date">${citiesDate()}</div>
+          </div>`;
     }
     setInterval(upd, 1000);
   }
