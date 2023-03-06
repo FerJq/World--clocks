154e5781d8ea162fdbc781e5b7defa94c2899e47
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-05 22:56:14 -0500

    Updated color Day time

--- src/app.js
+++ src/app.js
@@ -26,17 +26,18 @@ theme_bt.addEventListener("click", function (event) {
   if (body.classList.contains("darkmode")) {
     body.classList.remove("darkmode");
     event.preventDefault();
-    mode.innerHTML = `Night Time`;
+    mode.innerHTML = `Night`;
   } else {
     body.classList.add("darkmode");
     event.preventDefault();
-    mode.innerHTML = `Day time`;
+    mode.innerHTML = `Day`;
   }
 });
 
 function allCities() {
-  let pieceHTML = `<div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.gotokyo.org/en/index.html?utm_source=google&utm_medium=en_paidsearch&utm_campaign=R4&utm_content=tdA&gclid=CjwKCAiAmJGgBhAZEiwA1JZoll0h1q5NAst_q91FgmvzzP32cEvnm6pbqZ5l5foNPcOxx3ytyZEmvhoCn3cQAvD_BwE" target="_blank">🍣Tokyo</a></div>
+  let pieceHTML = `
+  <div class="cities-preview" id="cities-preview">
+            <div class="city_all" id="city"><a href="https://www.gotokyo.org/en/index.html?utm_source=google&utm_medium=en_paidsearch&utm_campaign=R4&utm_content=tdA&gclid=CjwKCAiAmJGgBhAZEiwA1JZoll0h1q5NAst_q91FgmvzzP32cEvnm6pbqZ5l5foNPcOxx3ytyZEmvhoCn3cQAvD_BwE" target="_blank">Tokyo</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(asia_tokyo)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -44,8 +45,9 @@ function allCities() {
               .tz(asia_tokyo)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://en.parisinfo.com" target="_blank">🥖Paris</a></div>
+            <div class="city_all" id="city"><a href="https://en.parisinfo.com" target="_blank">Paris</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(europe_paris)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -53,8 +55,9 @@ function allCities() {
               .tz(europe_paris)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.australia.com/en-us/places/sydney-and-surrounds/guide-to-sydney.html?cid=paid-search|us|SYD798|brand|Google||||||||||&ef_id=CjwKCAiAmJGgBhAZEiwA1JZollqEyBiX21W1RHR1lZA0bIiWX4zx0niKd2p3cw06hd8XLbCEaaIYxBoCHEgQAvD_BwE:G:s&s_kwcid=AL!4635!3!433828241982!e!!g!!sydney%20australia&utm_actcampaign=9514109454&gclid=CjwKCAiAmJGgBhAZEiwA1JZollqEyBiX21W1RHR1lZA0bIiWX4zx0niKd2p3cw06hd8XLbCEaaIYxBoCHEgQAvD_BwE" target="_blank">🐟Sydney</a></div>
+            <div class="city_all" id="city"><a href="https://www.australia.com/en-us/places/sydney-and-surrounds/guide-to-sydney.html?cid=paid-search|us|SYD798|brand|Google||||||||||&ef_id=CjwKCAiAmJGgBhAZEiwA1JZollqEyBiX21W1RHR1lZA0bIiWX4zx0niKd2p3cw06hd8XLbCEaaIYxBoCHEgQAvD_BwE:G:s&s_kwcid=AL!4635!3!433828241982!e!!g!!sydney%20australia&utm_actcampaign=9514109454&gclid=CjwKCAiAmJGgBhAZEiwA1JZollqEyBiX21W1RHR1lZA0bIiWX4zx0niKd2p3cw06hd8XLbCEaaIYxBoCHEgQAvD_BwE" target="_blank">Sydney</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(aus_sydn)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -62,8 +65,9 @@ function allCities() {
               .tz(aus_sydn)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.visitlisboa.com/" target="_blank">🥞Lisbon</a></div>
+            <div class="city_all" id="city"><a href="https://www.visitlisboa.com/" target="_blank">Lisbon</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(eu_lisbon)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -71,8 +75,9 @@ function allCities() {
               .tz(eu_lisbon)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.toronto.ca/" target="_blank">🍫Toronto</a></div>
+            <div class="city_all" id="city"><a href="https://www.toronto.ca/" target="_blank">Toronto</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(ame_toro)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -80,8 +85,9 @@ function allCities() {
               .tz(ame_toro)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://english.visitseoul.net/index" target="_blank">🍲Seoul</a></div>
+            <div class="city_all" id="city"><a href="https://english.visitseoul.net/index" target="_blank">Seoul</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(asia_seoul)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -89,8 +95,9 @@ function allCities() {
               .tz(asia_seoul)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.madeira.org/" target="_blank">☕Madeira</a></div>
+            <div class="city_all" id="city"><a href="https://www.madeira.org/" target="_blank">Madeira</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(at_mad)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -98,8 +105,9 @@ function allCities() {
               .tz(at_mad)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
           <div class="cities-preview" id="cities-preview">
-            <div class="city_all" id="city"><a href="https://www.iloveny.com/" target="_blank">🗽New York</a></div>
+            <div class="city_all" id="city"><a href="https://www.iloveny.com/" target="_blank">New York</a></div>
             <div class="hour_all" id="hour">${moment()
               .tz(ame_ny)
               .format("h:mm:ss [<small>]A[</small>]")}</div>
@@ -107,6 +115,7 @@ function allCities() {
               .tz(ame_ny)
               .format("MMMM Do YYYY")}</div>
           </div>
+          <hr>
 `;
   display_all.innerHTML = pieceHTML;
 }
@@ -143,8 +152,8 @@ dropdown.addEventListener("change", function (event) {
   if (values_selection.length > 0) {
     function upd() {
       let citiesname = values_selection.replace("_", " ").split("/")[1];
-      selected.innerHTML = "˜”*°•.City.•°*”˜";
-      displaycity.innerHTML = `<div class="cities-preview" id="displayAllCities">
+      displaycity.innerHTML = `<hr>
+      <div class="cities-preview" id="displayAllCities">
             <div class="cityy" id="city">${citiesname}</div>
             <div class="hourr" id="hour">${citiesTiming()}</div>
             <div class="datee" id="date">${citiesDate()}</div>
